3662f3e8931698f029fb28611e850d40d1228493
jbennett0411 <john_gassan@hotmail.com>
2016-10-25 14:18:13 -0600

    update

--- manifests/site.pp
+++ manifests/site.pp
@@ -52,7 +52,7 @@ node default {
 #  }
   exec { 'cowsay':
   command => "cowsay 'Welcome to ${::fqdn}!' > /etc/motd",
-  create  => '/etc/motd',
+  creates  => '/etc/motd',
   }
   # This is where you can declare classes for all nodes.
   # Example:
