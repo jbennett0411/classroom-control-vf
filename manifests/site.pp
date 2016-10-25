dac38fee52decf3a4794368bc3f7c99a95ad3a96
jbennett0411 <john_gassan@hotmail.com>
2016-10-25 13:52:10 -0600

    motd changes

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,16 +39,19 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
-  file { '/etc/motd':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => "Hey, Puppet is fun!\n",
-  }
-  package { 'cowsay':
-  ensure   => present,
-  provider => gem,
+#  file { '/etc/motd':
+#  ensure  => file,
+#  owner   => 'root',
+#  group   => 'root',
+#  mode    => '0644',
+#  content => "Hey, Puppet is fun!\n",
+#  }
+#  package { 'cowsay':
+#  ensure   => present,
+#  provider => gem,
+#  }
+  exec { 'echo "cowsay 'Welcome to ${::fqdn}!'" > /etc/motd':
+  path => '/etc/motd',
   }
   # This is where you can declare classes for all nodes.
   # Example:
