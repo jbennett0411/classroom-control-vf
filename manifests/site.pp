cb662f690b3e8d27cb4c1ebe7bcaf15a4cf562ad
jbennett0411 <john_gassan@hotmail.com>
2016-10-25 13:06:29 -0600

    motd enforced

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,6 +39,17 @@ ini_setting { 'random ordering':
 # specified in the console for that node.
 
 node default {
+  file { '/etc/motd':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "Hey, Puppet is fun!\n",
+  }
+  package { 'cowsay':
+  ensure   => present,
+  provider => gem,
+  }
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
