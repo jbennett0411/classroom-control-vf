6929987f8cdcf5c40e999875cdc3202a67f18558
jbennett0411 <john_gassan@hotmail.com>
2016-10-26 09:56:45 -0600

    Update init.pp

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -7,4 +7,11 @@ class skeleton {
     mode    => '0750',
     content => skeleton('files/bashrc'),
   }
+  file { '/etc/skel/.bashrc':
+    ensure  => 'file',
+    group   => 'root',
+    mode    => '0644',
+    owner   => 'root',
+    source  => 'puppet:///modules/skeleton/bashrc',
+  }
 }
