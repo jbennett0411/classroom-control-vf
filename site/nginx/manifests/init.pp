54750454e4a5ee6ba2e4061ea945a651b893be33
jbennett0411 <john_gassan@hotmail.com>
2016-10-26 13:37:58 -0600

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,25 @@
+class nginx{
+  package  => {'nginx':
+    ensure  => present,
+  }
+  file { '/etc/nginx/nginx.conf':
+    ensure   => file,
+    source   => 'puppet:///modules/nginx/nginx.conf',
+    require  => Package['nginx'],
+  }
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure   => file,
+    source   => 'puppet:///modules/nginx/default.conf',
+    require  => Package['nginx'],
+  }
+  file { '/var/www/index.html':
+    ensure   => file,
+    source   => 'puppet:///modules/nginx/index.html',
+    require  => Package['nginx'],
+  }
+  service {'nginx':
+    ensure     => running,
+    enable     => true,
+    subscribe  => File['/etc/nginx/nginx.conf'],
+  }
+}
