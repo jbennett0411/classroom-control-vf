90e40e8739aaa4235b9c1fc081a2ab1821ca639f
jbennett0411 <john_gassan@hotmail.com>
2016-10-27 12:53:34 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -68,9 +68,9 @@ node default {
 #  include skeleton
 include memcached
 include nginx
-class aliases {
-  $admin = 'root',
-}  
+#class aliases {
+#  $admin = 'root',
+#}  
 if $is_virtual == true {
     notify { 'This is a virtual machine.': }
   }
