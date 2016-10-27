28563cd58bf9b5125cdd8cff6cb36f99a63c5137
jbennett0411 <john_gassan@hotmail.com>
2016-10-27 08:59:48 -0600

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,19 +1,18 @@
 class nginx{
   File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
   }
   package {'nginx':
     ensure  => present,
   }
   file { '/etc/nginx/nginx.conf':
-    ensure   => file,
     source   => 'puppet:///modules/nginx/nginx.conf',
     require  => Package['nginx'],
   }
   file { '/etc/nginx/conf.d/default.conf':
-    ensure   => file,
     source   => 'puppet:///modules/nginx/default.conf',
     require  => Package['nginx'],
   }
@@ -21,7 +20,6 @@ class nginx{
     ensure  => directory,
   }
   file { '/var/www/index.html':
-    ensure   => file,
     source   => 'puppet:///modules/nginx/index.html',
   }
   service {'nginx':
