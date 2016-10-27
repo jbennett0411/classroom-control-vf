799edbd5a9be8614bab4ad86203e0d4debfdb862
jbennett0411 <john_gassan@hotmail.com>
2016-10-27 14:15:29 -0600

    Create managed_user.pp

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,20 @@
+#resource type
+define users::managed_user {
+  user { $title: 
+    ensure     => present,
+    managehome => true,
+  }
+  
+  file { "/home/$(title)/.ssh":
+    ensure => directory,
+    owner  => $title,
+    mode   => '0700',
+  }
+  
+  file { "/home/$title/.bashrc":
+    ensure => file,
+    mode   => '0644',
+    owner  => $title,
+    source => 'puppet:///modules/users/bashrc',
+  }
+}
